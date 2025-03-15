53c7b561fec723c14be4126dabee498056dfca6b
anggeryoga <139601862+anggeryoga@users.noreply.github.com>
2025-03-15 10:53:38 +0700

    @anggeryoga

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-echo "Menginstal dependencies..."
-pkg update && pkg upgrade -y
-pkg install python -y
-pip install requests scapy
-echo "Instalasi selesai. Jalankan dengan: python3 attack.py"
+
+# Update daftar paket Termux
+pkg update
+
+# Instal Python dan pip (jika belum terinstal)
+pkg install -y python
+
+# Instal dependensi Python menggunakan pip
+pip install scapy requests colorama
+
+# Instal nmap (optional)
+pkg install -y nmap
+
+# Konfirmasi Instalasi
+echo "Semua dependensi telah diinstal."
+echo "Anda sekarang dapat menjalankan attack.py."
